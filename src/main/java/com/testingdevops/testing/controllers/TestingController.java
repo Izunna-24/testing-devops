b63933aea75741cbb7e5abeeb813cf766f714d8c
izunna-24 <izuchukwuijeudo@gmail.com>
2024-09-03 15:02:07 +0100

    controller checked

--- src/main/java/com/testingdevops/testing/controllers/TestingController.java
+++ src/main/java/com/testingdevops/testing/controllers/TestingController.java
@@ -1,16 +1,16 @@
 package com.testingdevops.testing.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("api/hworld")
 @RestController
 public class TestingController {
 
-
-    public ResponseEntity<?> response(){
-        return ResponseEntity.ok("Hello World");
+    @GetMapping("/hworld")
+    public String sayHello() {
+        return "Hello World";
 
     }
 }
